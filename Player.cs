3b275c403c822506d9e88cdec39df19c57693847
Soufyan <MetaByte149@gmail.com>
2022-11-03 21:38:05 +0200

    Started small animation of player

--- Player.cs
+++ Player.cs
@@ -5,6 +5,9 @@ using Microsoft.Xna.Framework.Content;
 using System.Security.Cryptography;
 using System;
 using System.Diagnostics;
+using MonoGame.Extended.Animations;
+using System.Runtime.InteropServices;
+
 
 namespace soufGame;
 
@@ -17,14 +20,23 @@ internal class Player {
     private int direction;
     private bool walking;
 
+    private int animationIndex;
+    private int animationSpeed;
+
+
+
+
     public Player(ContentManager content) {
-        texture = content.Load<Texture2D>("player1");
+        texture = content.Load<Texture2D>("soufPlayer2");
         position = new Vector2(0, 0);
 
         idleTimer = 100;
         walkTimer = 0;
         direction = 1;
         walking = true;
+
+        animationIndex = 0;
+        animationSpeed = 5;
 
 
     }
@@ -52,7 +64,17 @@ internal class Player {
     }
 
     public void Draw(SpriteBatch spriteBatch) {
-        spriteBatch.Draw(texture, new Vector2(position.X, position.Y), Color.White);
+
+
+
+        var sourceRectangle = new Rectangle(animationIndex++ * 64, 704, 64, 64);
+        if (animationIndex >= 7) animationIndex = 0;
+        // Only draw the area contained within the sourceRectangle.
+        spriteBatch.Draw(texture, position, sourceRectangle, Color.White);
+
+
+        // Drawing the full thing
+        //spriteBatch.Draw(texture, new Vector2(position.X, position.Y), Color.White);
     }
 
 
